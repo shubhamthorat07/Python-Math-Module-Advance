f6f9477c444379b3a09492fcd4266efbdcf26c63
Shubham <40167163+shubhamthorat07@users.noreply.github.com>
2020-08-18 20:07:29 +0530

    Add files via upload

---
+++ mathadvance.py
@@ -0,0 +1,31 @@
+import math
+
+num2 = 14.77
+
+print(math.floor(num2), math.ceil(num2))
+
+
+print(math.sqrt(100))
+print(math.factorial(5))
+
+
+print(math.pow(20,2))
+
+print(math.isclose(2.2101,2.2100001))
+print(math.isclose(1.233,1.2330000001))
+
+
+
+import random
+print(random.random())
+
+
+
+l3 = [1,2,3,4,5,6,7,8,9]
+print(random.choice(l3))
+
+
+print(random.randint(100,200))
+
+
+
